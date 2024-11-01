63ac740261dc94deab0a61a7dac90ff437d266f1
dune123 <96974209+dune123@users.noreply.github.com>
2024-11-01 21:05:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+function addValue(){
+    let firstName=document.getElementById("firstName").value;
+    let secondName=document.getElementById("secondName").value;
+    let phNumebr=document.getElementById("phNumber").value;
+    let emailId=document.getElementById("emailID").value;
+
+    alert( "First Name: " + firstName + "\n" +
+      "Second Name: " + secondName + "\n" +
+      "Phone Number: " + phNumebr + "\n" +
+      "Email ID: " + emailId
+    )
+}
