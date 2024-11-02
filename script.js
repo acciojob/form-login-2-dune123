eec723d2c25f824f1dcf8dad1277fb6e1e40f534
dune123 <96974209+dune123@users.noreply.github.com>
2024-11-02 10:06:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //your JS code here. If required.
 
+
 function addValue(){
     let firstName=document.getElementById("firstName").value;
-    let secondName=document.getElementById("secondName").value;
+    let secondName=document.getElementById("lastName").value;
     let phNumebr=document.getElementById("phNumber").value;
     let emailId=document.getElementById("emailID").value;
 
